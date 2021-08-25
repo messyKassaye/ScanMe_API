a265ebf3dcddfdc5d3cb770d40f2dfd750509177
Meseret <nurelayinmehammed@gmail.com>
2021-08-24 19:07:22 -0700

    Global authorization is handled

--- ScanME/Middlewares/GlobalAuthenticationHandler.cs
+++ ScanME/Middlewares/GlobalAuthenticationHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,17 @@ namespace ScanME.Middlewares
 {
     public class GlobalAuthenticationHandler
     {
+        private readonly RequestDelegate _next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+
+            }catch(Exception ex)
+            {
+
+            }
+        }
     }
 }
