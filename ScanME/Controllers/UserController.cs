bc05241052ea207cadf8937a83cb570fd72c9ca4
Meseret <nurelayinmehammed@gmail.com>
2021-08-26 17:48:09 -0700

    User data {ME} is handled

--- ScanME/Controllers/UserController.cs
+++ ScanME/Controllers/UserController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ScanME.Contexts;
+using ScanME.DTO;
+using ScanME.UnitOfWorks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +13,27 @@ namespace ScanME.Controllers
     [ApiController]
     public class UserController : Controller
     {
+
+        public ApplicationUnitOfWork _unitOfWork;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _unitOfWork = new ApplicationUnitOfWork(context);
+        }
        
         [HttpGet("me")]
         public IActionResult Me()
         {
-            return Json(new { me = "Me" });
+            int UserId = (int)HttpContext.Items["UserId"];
+            var user = _unitOfWork.UserRepository.Show(UserId);
+            var userDTP= new UserDTO()
+            {
+                FullName=user.FullName,
+                Email=user.Email,
+                Phone=user.Phone
+            };
+
+            return Json(userDTP);
         }
     }
 }
--- ScanME/DTO/UserDTO.cs
+++ ScanME/DTO/UserDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScanME.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
